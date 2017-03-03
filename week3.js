d6cdb46e5fb1ad7f895208e4df9fcd9488cd1ff8
Jordan Fred <jcfred88@gmail.com>
2017-03-03 13:54:52 -0700

    reddit api get is working

--- week3.js
+++ week3.js
@@ -1,40 +1,47 @@
-var subreddits = ['funny', 'pics', 'AdviceAnimals','aww']
-for (var i = 0; i < subreddits.length; i++) {
+var subreddits = ['funny', 'pics', 'AdviceAnimals','aww','memes','earthporn','spaceporn']
+for (let i = 0; i < subreddits.length; i++) {
   var reddit = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/'+ subreddits[i] +'.json')
   reddit.done((data) => {
       if(reddit.status !== 200){
-        console.log("failed: " + subreddits[i])
-        return;
+        console.log("failed: " + i)
+        console.log(data)
+        //return;
       }
-      var isimage = false;
-      while(isimage == false){
-
-        var redditContent = data.data.children[2]
-        console.log(redditContent)
+      
+      //run while loop untill a post have a "jpg" or "png" picture url
+      var isImage = false;
+      var childPos = 1;
+      while(isImage == false){
+        childPos += 1;
+        var redditContent = data.data.children[childPos]
         var redditURL = redditContent.data.url
         var type = redditURL.substring(redditURL.length - 3,redditURL.length)
-        
-        var subName = redditContent.data.subreddit_name_prefixed;
-        console.log(subName);
-        var postTitle = redditContent.data.title;
-        console.log(postTitle)
-        console.log(redditURL);
-        var redditComment = redditContent.data.permalink;
-        var comments = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
-        comments.done((foo) => {
-            if(comments.status !== 200){
-              console.log("comments failed: " + subreddits[i])
-              return;
-            }
-            var topComment = foo[1].data.children[0].data.body
-            console.log(topComment)
-            displayContent(subName,postTitle,redditURL,topComment);
-
-        })
+        if(type === "jpg" || type === "png"){
+          isImage = true;
+        } else {console.log("failed: " + type)}
       }
+      //get the rest of information - subreddit, title, top comment
+      var subName = redditContent.data.subreddit_name_prefixed;
+      //console.log(subName);
+      var postTitle = redditContent.data.title;
+      //console.log(postTitle)
+      //console.log(redditURL);
+      var redditComment = redditContent.data.permalink;
+      var comments = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
+      comments.done((foo) => {
+          if(comments.status !== 200){
+            console.log("comments failed: " + subreddits[i])
+            return;
+          }
+          var topComment = foo[1].data.children[0].data.body
+          //console.log(topComment)
+          displayContent(subName,postTitle,redditURL,topComment);
+
+      })
     })
 }
 
+//function to append information into display div
 function displayContent(subred, title, post, comment){
   $('#display').append("<div class='weed'><p>"+subred+"</p><p>"+title+"</p><img src='"+post+"'><p>"+comment+"</p></div>")
 }
