1f2e06402a6b29985274be308652173eb1d29f2a
Jordan Fred <jcfred88@gmail.com>
2017-03-03 12:39:39 -0700

    more stuff added

--- week3.js
+++ week3.js
@@ -1,28 +1,37 @@
-var subreddits = ['gifs', 'pics', 'AdviceAnimals','reactiongifs']
+var subreddits = ['funny', 'pics', 'AdviceAnimals','aww']
 for (var i = 0; i < subreddits.length; i++) {
   var reddit = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/'+ subreddits[i] +'.json')
   reddit.done((data) => {
       if(reddit.status !== 200){
+        console.log("failed: " + subreddits[i])
         return;
       }
-      var redditContent = data.data.children[2]
-      console.log(redditContent)
-      var subName = redditContent.data.subreddit_name_prefixed;
-      console.log(subName);
-      var postTitle = redditContent.data.title;
-      console.log(postTitle)
-      var redditURL = redditContent.data.url
-      console.log(redditURL);
-      var redditComment = redditContent.data.permalink;
-      var comments = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
-      comments.done((foo) => {
-          if(comments.status !== 200){
-            return;
-          }
-          var topComment = foo[1].data.children[0].data.body
-          console.log(topComment)
-          displayContent(subName,postTitle,redditURL,topComment);
-      })
+      var isimage = false;
+      while(isimage == false){
+
+        var redditContent = data.data.children[2]
+        console.log(redditContent)
+        var redditURL = redditContent.data.url
+        var type = redditURL.substring(redditURL.length - 3,redditURL.length)
+        
+        var subName = redditContent.data.subreddit_name_prefixed;
+        console.log(subName);
+        var postTitle = redditContent.data.title;
+        console.log(postTitle)
+        console.log(redditURL);
+        var redditComment = redditContent.data.permalink;
+        var comments = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
+        comments.done((foo) => {
+            if(comments.status !== 200){
+              console.log("comments failed: " + subreddits[i])
+              return;
+            }
+            var topComment = foo[1].data.children[0].data.body
+            console.log(topComment)
+            displayContent(subName,postTitle,redditURL,topComment);
+
+        })
+      }
     })
 }
 
