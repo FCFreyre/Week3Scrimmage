263ec03fa5f9a3afa5bd0572304e1c440e91c4b9
Jordan Fred <jcfred88@gmail.com>
2017-03-03 11:58:24 -0700

    getting more content and starting to display it

--- week3.js
+++ week3.js
@@ -1,23 +1,31 @@
-var reddit = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/gifs.json')
-reddit.done((data) => {
-    if(reddit.status !== 200){
-      return;
-    }
-    console.log(data);
-
-    var redditContent = data.data.children[2]
-    var redditURL = ""
-    redditURL = redditContent.data.url
-    redditComment = redditContent.data.permalink
-    var commentTop = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
-    commentTop.done((com) => {
-        if(commentTop.status !== 200){
-          return;
-        }
-        console.log(com);
-        var foo = com[1].data.children[0].data.body
-        console.log(foo)
-
+var subreddits = ['gifs', 'pics', 'AdviceAnimals','reactiongifs']
+for (var i = 0; i < subreddits.length; i++) {
+  var reddit = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/'+ subreddits[i] +'.json')
+  reddit.done((data) => {
+      if(reddit.status !== 200){
+        return;
+      }
+      var redditContent = data.data.children[2]
+      console.log(redditContent)
+      var subName = redditContent.data.subreddit_name_prefixed;
+      console.log(subName);
+      var postTitle = redditContent.data.title;
+      console.log(postTitle)
+      var redditURL = redditContent.data.url
+      console.log(redditURL);
+      var redditComment = redditContent.data.permalink;
+      var comments = $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com' + redditComment + '.json')
+      comments.done((foo) => {
+          if(comments.status !== 200){
+            return;
+          }
+          var topComment = foo[1].data.children[0].data.body
+          console.log(topComment)
+          displayContent(subName,postTitle,redditURL,topComment);
+      })
     })
+}
 
-  })
+function displayContent(subred, title, post, comment){
+  $('#display').append("<div class='weed'><p>"+subred+"</p><p>"+title+"</p><img src='"+post+"'><p>"+comment+"</p></div>")
+}
